72b708df01020af0013ab03498cfa8d97f3ae93a
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-05-04 14:59:55 +0300

    Меняю все логические сравнения (and, not, or и тд) на их знаковые эквиваленты (&&, !, || и тд)

--- morpho/morpho/main.cpp
+++ morpho/morpho/main.cpp
@@ -1,15 +1,31 @@
 // morpho.cpp : main project file.
 
+#include <sstream>
 #include "stdafx.h"
 #include "morpho.h"
+#include <iostream>
+#include "encode/utf8_iterator.h"
+#include "alphabets/rus_alphabet.h"
+#include "alphabets/eng_alphabet.h"
+#include "morpho_modifier.h"
+#include "morpho.h"
+#include "models/rus_model.h"
+#include "models/eng_model.h"
+#include "models/rus_model_description.h"
+#include "models/eng_model_description.h"
+
 
 using namespace System;
 using namespace strutext;
+using namespace strutext::morpho;
 
 int main(array<System::String ^> ^args)
 {
 
-	//morpho::Morphologist morphologist;
+	Morphologist<RussianAlphabet> morphologist;
+	MorphologistBase::LemList lem_list;
+	std::string testStr = "мамы";
+	morphologist.Analize(testStr, lem_list);
 
     Console::WriteLine(L"Hello World");
 	Console::ReadLine();
