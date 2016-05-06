e5bfd32c0738228f2dc00aa56404d7019473dbd6
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-05-06 13:37:05 +0300

    Strutext компилится без ошибок, пытаюсь запустить тестовый пример.

--- StrutextConsoleApplication/StrutextConsoleApplication/StrutextConsoleApplication.cpp
+++ StrutextConsoleApplication/StrutextConsoleApplication/StrutextConsoleApplication.cpp
@@ -4,14 +4,25 @@
 #include <boost/regex.hpp>
 #include <iostream>
 #include <string>
+#include "morpho/morpho.h"
+#include "encode/utf8_iterator.h"
+#include "alphabets/rus_alphabet.h"
+#include "alphabets/eng_alphabet.h"
+#include "morpho/morpho_modifier.h"
+#include "models/rus_model.h"
+#include "models/eng_model.h"
+#include "models/rus_model_description.h"
+#include "models/eng_model_description.h"
 
 using namespace std;
 using namespace System;
+using namespace strutext;
+namespace m = strutext::morpho;
 
 int main()
 {
 	std::string line;
-	line = "124124124";
+	line = "Re: 124124124";
 	boost::regex pat("^Subject: (Re: |Aw: )*(.*)");
 	boost::smatch matches;
 
@@ -22,6 +33,20 @@ int main()
 	else {
 		Console::WriteLine(L"String dont match!");
 	}
+
+
+	typedef m::Morphologist<m::RussianAlphabet> Morpher;
+	Morpher morpher;
+	Morpher::LemList lem_list;
+	std::string testStr = "мамы";
+	morpher.Analize(testStr, lem_list);
+
+	for (Morpher::LemList::iterator it = lem_list.begin(); it != lem_list.end(); ++it) {
+	
+		Console::WriteLine(L"123412");
+	
+	}
+
     Console::WriteLine(L"Hello World");
 	Console::ReadLine();
     return 0;
