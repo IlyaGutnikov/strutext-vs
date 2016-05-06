8aa8f9d5c7069332d119dc3125e1ebd2b441a553
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-05-06 13:01:11 +0300

    В проекте StrutextConsoleApplication - рабочий пример работы и линкирования библиотеки boostlibs

--- StrutextConsoleApplication/StrutextConsoleApplication/StrutextConsoleApplication.cpp
+++ StrutextConsoleApplication/StrutextConsoleApplication/StrutextConsoleApplication.cpp
@@ -1,11 +1,27 @@
 // StrutextConsoleApplication.cpp : main project file.
 
 #include "stdafx.h"
+#include <boost/regex.hpp>
+#include <iostream>
+#include <string>
 
+using namespace std;
 using namespace System;
 
-int main(array<System::String ^> ^args)
+int main()
 {
+	std::string line;
+	line = "124124124";
+	boost::regex pat("^Subject: (Re: |Aw: )*(.*)");
+	boost::smatch matches;
+
+	if (boost::regex_match(line, matches, pat)) {
+		Console::WriteLine(L"String match!");
+		std::cout << matches[2] << std::endl;
+	}
+	else {
+		Console::WriteLine(L"String dont match!");
+	}
     Console::WriteLine(L"Hello World");
 	Console::ReadLine();
     return 0;
