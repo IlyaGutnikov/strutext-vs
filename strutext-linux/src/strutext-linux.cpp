6cf1361a832cf05250e81cad0dff05ec1a120300
ilyagutnikov <ilyagutnikov93@gmail.com>
2016-05-13 10:10:44 +0300

    Делаю простой проект в котором запускаю strutext в Eclipse for CPP, на Ubuntu 14.04

---
+++ strutext-linux/src/strutext-linux.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : strutext-linux.cpp
+// Author      : IlyaGutnikov
+// Version     :
+// Copyright   : 123
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <boost/regex.hpp>
+using namespace std;
+
+int main() {
+	boost::smatch matches;
+	std::string line;
+	boost::regex pat();
+
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
